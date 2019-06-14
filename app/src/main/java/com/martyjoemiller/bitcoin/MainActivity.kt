90af6f8063793e6019c350018d08f7f5adc94f9a
martymiller <martymiller@users.noreply.github.com>
2019-06-14 03:30:37 -0400

    initial add

---
+++ app/src/main/java/com/martyjoemiller/bitcoin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.martyjoemiller.bitcoin
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.functions.Consumer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun init() {
+    }
+}
+
+
